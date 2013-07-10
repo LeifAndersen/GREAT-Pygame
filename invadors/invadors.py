10c7046718a9548c9e4ff6c3f94546cec7d15cba
Leif Andersen <leif@leifandersen.net>
2013-07-10 16:19:06 -0600

    Added sound.

--- invadors/invadors.py
+++ invadors/invadors.py
@@ -6,7 +6,7 @@ move_right = False
 move_up    = False
 move_down  = False
 
-resolution = width, height = 500, 500
+resolution = width, height = 700, 700
 
 # Your ship
 shipSprite = pygame.image.load("ship.jpg")
@@ -14,7 +14,7 @@ ship = shipSprite.get_rect()
 
 # Enemy Ships
 enemySprite = pygame.image.load("enemy.jpg")
-enemies = [enemySprite.get_rect() for i in range(8)]
+enemies = [enemySprite.get_rect() for i in range(100)]
 enemyVelocities = [1 for enemy in enemies]
 
 # Lazers
@@ -26,19 +26,31 @@ screen = pygame.display.set_mode(resolution)
 black = 0, 0, 0
 white = 255, 255, 255
 
+# Music
+pygame.mixer.init()
+pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play(0)
+
+explosion = pygame.mixer.Sound("explosion1.wav")
+pew = pygame.mixer.Sound("pew1.wav")
+
 def end_game():
     pygame.quit()
     sys.exit()
 
 def init():
     # Setup Ship
-    ship.x = 200
-    ship.y = 400
+    ship.x = 350
+    ship.y = 600
     x = 0
+    y = 10
     for enemy in enemies:
         enemy.x = x
-        enemy.y = 10
+        enemy.y = y
         x += 50
+        if x > 650:
+            x = 0
+            y += 40
 
 def create_laser(location):
     laser = laserSprite.get_rect()
@@ -72,6 +84,7 @@ def events():
             if event.key == pygame.K_ESCAPE:
                 end_game()
             if event.key == pygame.K_SPACE:
+                pew.play()
                 lasers.append(create_laser(ship))
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -97,8 +110,13 @@ def move():
     # Set Enemy Velocity
     i = 0
     for enemy in enemies:
-        if enemy.x > width - 32 or enemy.x < 0:
-            enemyVelocities[i] *= -1
+        if enemy.x > width - 32:
+            enemyVelocities[i] = -1
+            enemy.x = width - 33
+            enemy.y += 30
+        if enemy.x < 0:
+            enemyVelocities[i] = 1
+            enemy.x = 1
             enemy.y += 30
         i += 1
 
@@ -115,15 +133,22 @@ def move():
     # Check to see if ship has collided/game over
     for enemy in enemies:
         if enemy.colliderect(ship) or enemy.y > height:
+            explosion.play()
             end_game()
 
     # Laser Enemy Collisions
     for laser in lasers[:]:
         for enemy in enemies[:]:
             if laser.colliderect(enemy):
-                lasers.remove(laser)
-                enemies.remove(enemy)
-
+                explosion.play()
+                try:
+                    lasers.remove(laser)
+                except:
+                    pass
+                try:
+                    enemies.remove(enemy)
+                except:
+                    pass
     # Check to see if won
     if len(enemies) == 0:
         end_game()
